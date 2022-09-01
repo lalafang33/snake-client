696d4788e63f48abb35d10dc5faee84e46f0062a
Lalafang33 <lalafang33@gmail.com>
2022-08-31 18:40:56 -0700

    first commit

--- play.js
+++ play.js
@@ -1,19 +1,6 @@
 const {connect} = require("./client");
-
-const handleUserInput = function (data) {
-  if (key === '\u0003') {
-    process.exit();
-  }
-};
-
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  stdin.on("data", handleUserInput);
-  return stdin;
-};
+const {setupInput} = require("./input");
 
 console.log("Connecting ...");
 connect();
+setupInput();

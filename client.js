fdb39750d72ff41da0c3f59aad19c1e4924cb9bc
Lalafang33 <lalafang33@gmail.com>
2022-08-31 21:10:48 -0700

    first commit

--- client.js
+++ client.js
@@ -1,34 +1,20 @@
 const net = require("net");
 const { setupInput } = require("./input");
+const { IP, PORT } = require("./constant");
 const connect = function () {
   const conn = net.createConnection({
     host: '192.168.50.206', // IP address here,
     port: 50541, // PORT number here,
   });
 
-  // interpret incoming data as text
-  // const encoding = 
+
   conn.setEncoding("utf8");
 
-  // process.stdin.on('data', (message) => {
-  //   conn.write(message);
-  // });
 
-  // const connected = 
   conn.on('connect', () => {
     conn.write(`Name: SNK`);
     console.log('Successfully connected to game server');
    });
-
-   const send = (command) => {
-    conn.on('connect', () => {
-      conn.write(command);
-    })
-  }
-
-  //  conn.on('connect', () => {
-  //   conn.write('I love this game!');
-  //  });
 
    conn.on('connect', () => setupInput(conn));
 
